0f3abc4ff461e3704239262c4eaf5af3547d18fc
Elijah <72550846+ElijahBretbacher@users.noreply.github.com>
2022-04-28 09:56:11 +0200

    no no no no yes no no no no no no

--- background_process.py
+++ background_process.py
@@ -2,31 +2,42 @@ import psutil
 
 
 
-def initialize(data, settings):
+def initialize(data, settings, boolArr):
     with open("data.cnf", "r") as dataFile:
         tmpData = dataFile.readlines()
     for line in tmpData:
-        splitLine = line.split(";").pop(0)
+        splitLine = line.split(";")
+        splitLine.pop(0)
+        print(splitLine)
         data.append(splitLine)
+        boolArr.append(False)
 
     with open("settings.cnf", "r") as settingsFile:
         settings = settingsFile.readline().split(",")
 
 
 def displayOverlay():
+    print("Yes")
     pass
 
 
 # variables
 data = []
 settings = []
+boolArr = []
 
 # code
-initialize(data=data, settings=settings)
+initialize(data=data, settings=settings, boolArr=boolArr)
 print(data)
 
 # start loop
 while(True):
+    i = 0
     for arr in data:
-        if arr[0] in (i.name() for i in psutil.process_iter()):
+        # if the exe is running and it hasnt been open before
+        if arr[0] in (it.name() for it in psutil.process_iter()) and not boolArr[i]:
+            boolArr[i] = True
             displayOverlay()
+        else:
+            print("no")
+        i += 1
